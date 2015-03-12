f1b807896e950154592a91b9d1b3c5a9e403a2d9
Miguel Ibero <miguel@ibero.me>
2015-03-12 22:22:13 +0100

    updated buffer

---
+++ src/gorn/base/Config.hpp
@@ -0,0 +1,12 @@
+#ifndef __gorn__Config__
+#define __gorn__Config__
+
+#ifndef _MSC_VER
+#define NOEXCEPT noexcept
+#define THROW    throw()
+#else
+#define NOEXCEPT
+#define THROW
+#endif
+
+#endif
